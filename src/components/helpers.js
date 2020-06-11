7b8d6497dd6b49dcdc4ba9dee73a1c2f5fba3725
jeroen <jenyckee@MacBook-Pro-de-jeroen.local>
2020-06-11 23:09:44 +0200

    fix

--- src/components/helpers.js
+++ src/components/helpers.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
 export const format = (text) => {
-  if (text && text.split)
+  if (text && text.split) {
     const formatted = text.split(`\n\n`).map(paragraph => `<p>${paragraph.replace(/\n/g, `<br>`)}</p>`).join(``)
-  else return
-    <p>{text}</p>
-  return <div dangerouslySetInnerHTML={{ __html: formatted }} />
+    return <div dangerouslySetInnerHTML={{ __html: formatted }} />
+  } 
+  return (<p>{text}</p>)
 }

f63bc44238cc1d91be6294480a982d8c6d2a0bec
jeroen <jenyckee@MacBook-Pro-de-jeroen.local>
2020-06-12 11:38:09 +0200

    thanks

--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Layout from '../../components/Layout'
+import { Link } from 'gatsby'
 
 export default () => (
   <Layout>
     <section className="section">
       <div className="container">
         <div className="content">
-          <h1>Bedankt en tot binnenkort!</h1>
+          <h1>Bedankt en tot binnenkort! </h1>
+          <Link to="/">Ga terug</Link>
         </div>
       </div>
     </section>

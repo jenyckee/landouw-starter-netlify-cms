a756287093ef50481fbbfe566d0d9d6616f78428
jeroen <jenyckee@MacBook-Pro-de-jeroen.local>
2020-06-12 12:22:02 +0200

    title and description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Landouw',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Landouw - Restaurant - Grill - Tearoom - Een plaats waar het gezellig mag zijn.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
